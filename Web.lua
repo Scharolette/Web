ea10d02d885bd099b81c57ad4224d97945b171d0
Scharolette <116415549+Scharolette@users.noreply.github.com>
2022-10-22 20:40:24 -0400

    Update Web.lua

--- Web.lua
+++ Web.lua
@@ -2,22 +2,22 @@
 
 local supportedGames = {
     [4188447592] = function() -- Cat Simulator
-        loadstring("https://raw.githubusercontent.com/Scharolette/Web/main/Source/CatSimulator4188447592.lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Scharolette/Web/main/Source/CatSimulator4188447592.lua"))()
     end,
     [6149941304] = function() -- Chicken Life
-        loadstring("https://raw.githubusercontent.com/Scharolette/Web/main/Source/ChickenLife6149941304.lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Scharolette/Web/main/Source/ChickenLife6149941304.lua"))()
     end,
     [10675042838] = function() -- Lemonade Tycoon
-        loadstring("https://raw.githubusercontent.com/Scharolette/Web/main/Source/LemonadeTycoon10675042838.lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Scharolette/Web/main/Source/LemonadeTycoon10675042838.lua"))()
     end,
     [9285238704] = function() -- Race Clicker
-        loadstring("https://raw.githubusercontent.com/Scharolette/Web/main/Source/RaceClicker9285238704.lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Scharolette/Web/main/Source/RaceClicker9285238704.lua"))()
     end,
-    [4183364845] = function() -- Speed Run 4
-        loadstring("https://raw.githubusercontent.com/Scharolette/Web/main/Source/SpeedRun4183364845.lua")
+    [4183364845] = function() -- Speed Run 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Scharolette/Web/main/Source/SpeedRun4183364845.lua"))()
     end,
     [9498006165] = function() -- Tapping Simulator
-        loadstring("https://raw.githubusercontent.com/Scharolette/Web/main/Source/TappingSimulator9498006165.lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Scharolette/Web/main/Source/TappingSimulator9498006165.lua"))()
     end
 }
 
